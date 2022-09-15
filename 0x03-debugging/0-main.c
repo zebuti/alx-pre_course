7b0cf2fad8ae6ef3fd64e9257e8374a3997af59e
zebuti <zebuti@example.com>
2022-09-15 18:41:20 +0300

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - test function that prints if integer is positive or negative
+* RETURN: 0
+*/
+
+int main(void)
+{
+int i;
+
+i=0;
+positive_or_negative()[Di[C);
+return (0);
+}
